7f4e6089346330ee52419cddbc4f7bee0cbc4f7a
minhtien213 <minhtien213@gmail.com>
2024-01-23 22:12:30 +0900

    add checbox button cart page

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,11 +1,21 @@
 
 const moment = require('moment')
+const randomstring = require('randomstring')
+const nodemailer = require('nodemailer')
 const Product = require("../models/Product")
 const checkPermissionMiddlewares = require('../middlewares/checkPermissionMiddlewares')
 const { mongooseToObject, mutipleMongooseToObject } = require('../../util/mongoose')
-const randomstring = require('randomstring')
 const Order = require('../models/Order')
 const Account = require('../models/Account')
+
+// Cấu hình Nodemailer
+// const transporter = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: 'minhtien213tv@gmail.com',
+//     pass: 'xxxxxxxxxx',
+//   },
+// })
 
 class OrderController {
   
@@ -43,35 +53,56 @@ class OrderController {
 
       
     //[POST] /add-order
-    async addOrder(req, res, next) {
-      const _this = this
+    addOrder(req, res, next) {
       checkPermissionMiddlewares(req, res, [0, 1], (account) => {
         const orderCode = randomstring.generate({
           length: 8,  // Độ dài của mã đơn hàng
           charset: 'alphanumeric'  // Sử dụng các ký tự và số
         }).toUpperCase()
         const data = req.body
-        
+        console.log(data)
+        const productIds = data.products.map(product => product.productId)
         //xóa product đã mua trong cart
-        data.products.forEach(product => {
-          Account.updateMany(
-            { 'cart.product': product.productId },
-            { $pull: { cart: { product: product.productId } } }
+          Account.findOneAndUpdate(
+            { _id: account._id },
+            { $pull: { cart: { product: { $in: productIds } } } },
+            { new: true },
           )
-          .then(() => console.log(`Đã xóa sản phẩm.`))
+          .then((account) => {
+            const orderData = {
+              ...data,
+              orderProducts: data.products,
+              account: account._id,
+              orderCode,
+              orderStatus: 'Successed',
+              addedAt: moment().format('DD/MM/YYYY'),
+            }
+            const order = new Order(orderData)
+            order.save()
+              .then((order) =>{
+                // console.log(order)
+                // // Gửi email
+                // const mailOptions = {
+                //   from: 'minhtien213tv@gmail.com',
+                //   to: 'minhtien213@gmail.com',
+                //   subject: 'Đơn hàng của bạn đã được hoàn tất',
+                //   text: 'Cảm ơn bạn đã đặt hàng. Đơn hàng của bạn đã được hoàn tất.',
+                // }
+                // transporter.sendMail(mailOptions, (error, info) => {
+                //   if (error) {
+                //     console.error(error);
+                //     return res.status(500).send('Đã có lỗi xảy ra khi gửi email.')
+                //   } else {
+                //     console.log('Email sent: ' + info.response);
+                //     // Thực hiện các lệnh phản hồi khác sau khi gửi email thành công
+                //     res.status(200).send('Đơn hàng đã được hoàn tất và email đã được gửi.')
+                //   }
+                // })
+                res.json({ success: true, redirect: `/order/order-detail/${order._id}` })
+              })
+              .catch(next)
+          })
           .catch(error => console.error(`Lỗi khi xóa sản phẩm`))
-        })
-        
-        const orderData = {
-          ...data,
-          orderProducts: data.products,
-          account: account._id,
-          orderCode,
-          orderStatus: 'Successed',
-          addedAt: moment().format('DD/MM/YYYY'),
-        }
-        const order = new Order(orderData)
-        order.save()
       })
     }
 
@@ -80,7 +111,6 @@ class OrderController {
       checkPermissionMiddlewares(req, res, [0, 1], (account) => {
         Order.find({ account: account._id }).populate('orderProducts.productId')
           .then((orderLists) => {
-            // console.log(orderLists)
             res.render('order/my-orders', {
               orderLists: mutipleMongooseToObject(orderLists),
               account: mongooseToObject(account),
